e672042fae1c0417d05055584cbff84ab287dc57
Bruno Rodrigues Marciano <bruno-9237@hotmail.com>
2019-10-21 11:14:49 -0300

    Corrigido ícones para ter o fundo transparente e ajustado cores do aplicativo. close #24

--- src/WeStop.Web/gulpfile.js
+++ src/WeStop.Web/gulpfile.js
@@ -10,7 +10,7 @@ const runSequence = require('run-sequence');
 const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 const merge2 = require('merge2');
-//const pwaAssetGenerator = require('pwa-asset-generator');
+const pwaAssetGenerator = require('pwa-asset-generator');
 
 gulp.task('clean', function () {
     return gulp.src('dist/')
@@ -110,7 +110,7 @@ gulp.task('generate-pwa-assets', function () {
             'E:/WeStop/WeStop/src/WeStop.Web/images/stop.png',
             'E:/WeStop/WeStop/src/WeStop.Web/icons',
             {
-                scrape: false,
+                opaque: false,
                 log: true
             });
     })();
